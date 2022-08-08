9149a39d205d51c972de39b7d19f4ab8b6c95792
Juan Pablo Tovar <72332156+juanpablotovarvallejo@users.noreply.github.com>
2022-08-08 16:27:17 -0500

    Feature / Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './rutas/inicio/inicio.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
